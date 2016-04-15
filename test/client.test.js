e6977643b36729c1652445db5dc549260d989583
Jared Hanson <jaredhanson@gmail.com>
2016-04-15 08:16:54 -0300

    Add test cases.

--- test/client.test.js
+++ test/client.test.js
@@ -1,6 +1,8 @@
 /* global describe, it */
 
 var factory = require('../lib/client');
+var $require = require('proxyquire');
+var sinon = require('sinon');
 var expect = require('chai').expect;
 
 
@@ -16,12 +18,26 @@ describe('vault/client', function() {
     expect(factory['@require']).to.deep.equal([ '$settings' ]);
   });
   
-  describe.skip('created object', function() {
+  describe('created object', function() {
     var client;
+    var spyOnVault = sinon.spy(require('node-vault'));
     
     before(function() {
-      client = factory();
+      var settings = { get: function(){} };
+      var stub = sinon.stub(settings, 'get')
+        .withArgs('url').returns('http://vault.example.test:8200')
+        .withArgs('token').returns('keyboardcat');
+      
+      var factory = $require('../lib/client', { 'node-vault': spyOnVault });
+      client = factory(settings);
     })
+    
+    it('should be created with options from settings', function() {
+      expect(spyOnVault).to.have.been.calledWith({
+        endpoint: 'http://vault.example.test:8200',
+        token: 'keyboardcat'
+      });
+    });
     
     it('should conform to interface', function() {
       expect(client).to.be.an('object');
