0b3c972adeab455d6ec06a6e040016bd777227fb
Jared Hanson <jaredhanson@gmail.com>
2016-09-15 14:39:56 -0700

    Simplify spec manifest.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-exports = module.exports = function vault(id) {
-  var map = {
-    'client': './xom/client'
-  };
-  
-  var mid = map[id];
-  if (mid) {
-    return require(mid);
-  }
-};
-
-exports.used = function(container) {
-  container.add('client');
+exports = module.exports = {
+  'client': require('./xom/client')
 };
